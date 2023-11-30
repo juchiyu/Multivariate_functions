34d513c64757f862b08dfe068746fb218336b03e
Ju-Chi Yu <23201134+juchiyu@users.noreply.github.com>
2023-11-30 14:40:22 -0500

    Add supplementary projection and k-fold cross validation for PLSC

---
+++ PLSC-related/HowToDo10FoldCV.R
@@ -0,0 +1,21 @@
+### 10-fold cross validation for PLSC ###
+### Author: Ju-Chi Yu
+### Date: Nov. 30th, 2023
+##---------------------------------------
+
+## Reading packages ----
+library(TExPosition) # to run PLSC
+library(purrr) # used in the kfold function
+
+## Read needed functions ----
+
+## Read example data ----
+data(beer.tasting.notes)
+data1<-beer.tasting.notes$data[,1:8]
+data2<-beer.tasting.notes$data[,9:16]
+
+## Run PLSC ----
+pls.res <- tepPLS(data1,data2, graphs = FALSE)
+
+## Perform 10-fold validation
+pls.cv <- PLS.kFoldCV(data1, data2, pls.res, k = 10)
